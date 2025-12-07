00f737c11c8a87ff7393a900c4e20133e5860536
Steven-Fake <117360034+Steven-Fake@users.noreply.github.com>
2025-12-07 19:27:02 +0800

    fix a bug

--- plugins/MoreLinkIcons/MoreLinkIcons.js
+++ plugins/MoreLinkIcons/MoreLinkIcons.js
@@ -4,7 +4,7 @@
     var api = window.PluginApi;
     var { React, ReactDOM, libraries, patch, components } = api;
     var { faLink } = libraries.FontAwesomeSolid;
-    var customAssetPath = "./plugin/moreLinkIcons/assets/custom";
+    var customAssetPath = "./plugin/MoreLinkIcons/custom";
     var customDefinitionsPath = `${customAssetPath}/index.json`;
 
     // src/types/LinkDefinitions.ts
